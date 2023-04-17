fe7519189ca00c29bde9171ac799fb1f9f2fdde5
ericpark25 <ericpark025@gmail.com>
2023-04-17 19:22:45 -0400

    started working on the routes

---
+++ routes/user.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+const router = Router();
+import {userData} from '..data/users.js';
+
+router.route("/").get(async (req, res) => {
+  return res.json({ error: "No user id provided" });
+});
+
+router.route("/:id").get(async (req, res) => {
+    try {
+        let id = req.params.id;
+        if (!id) throw 'Error: You must procvide a ID';
+        if (typeof id !== 'string') throw 'Error: ID must be a string';
+        id = id.trim();
+        if (id.length === 0)
+            throw 'Error: ID cannot be an empty string or just spaces';
+        if (!ObjectId.isvalid(id)) throw 'Error: ID is an invalid object ID';
+    } catch (e) {
+        return res.status(400).json({error: e});
+    }
+    try {
+        let user = await userData.get(id);
+        res.json(user);
+    } catch (e) {
+        res.status(404).json({error: 'User not found'});
+    }
+});
+
+export default router;
